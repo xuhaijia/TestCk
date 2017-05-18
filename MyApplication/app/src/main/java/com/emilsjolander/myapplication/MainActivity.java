ebc59b9409dec5926a944febc12111a5613563a2
xuhaijia <sevenjiaup@163.com>
2017-05-18 11:37:04 +0800

    12
    12312

--- MyApplication/app/src/main/java/com/emilsjolander/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/emilsjolander/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "使用仓库", Toast.LENGTH_SHORT).show();
+        //分支
     }
 }
